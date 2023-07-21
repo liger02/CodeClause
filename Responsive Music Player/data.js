479e5442a0c068b11b3e00ab2047b414cd12c1a5
liger02 <126306820+liger02@users.noreply.github.com>
2023-07-21 13:18:15 +0530

    Create data.js
    java script file of songs is added

---
+++ Responsive Music Player/data.js
@@ -0,0 +1,32 @@
+let songs =[
+    {
+        name : 'Jhoome Jo Pathaan',
+        path : 'Jhoome_Jo_Pathaan_Song___Shah_Rukh_Khan_Deepika__.mp3',
+        artist: 'Arijit Singh & Sukriti Kakar',
+        cover : 'image_1.png'
+    },
+    {
+        name : 'Khwaja Mere Khwaja',
+        path : 'Khwaja_Mere_Khwaja_Jodhaa_Akbar_@A_R_Rahman_Hr.mp3',
+        artist: 'A.R. Rahman',
+        cover : 'image_2.png'
+    },
+    {
+        name : 'Sajdaa',
+        path : 'Sajdaa_Full_Video_My_Name_is_Khan_Shahrukh_Khan_.mp3',
+        artist: 'Nusrat Fateh Ali Khan , Rahat Fateh Ali Khan',
+        cover : 'image_3.png'
+    },
+    {
+        name : 'Kal Ho Naa Ho',
+        path : 'Kal_Ho_Naa_Ho_Full_Video_Title_Track_Shah_Rukh_K.mp3',
+        artist: 'Sonu Nigam',
+        cover : 'image_6.png'
+    },
+    {
+        name : 'Kesariya',
+        path : 'Kesariya_Brahmāstra___Ranbir_Kapoor___Alia_Bhatt.mp3',
+        artist: 'Arijit Singh',
+        cover : 'image_4.png'
+    },
+]
